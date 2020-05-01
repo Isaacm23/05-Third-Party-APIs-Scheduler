903ba80122e80b9ecb884552fd613a851e2d864c
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-05-01 04:47:47 -0700

    time for time blocks are all set

--- java.js
+++ java.js
@@ -1,4 +1,23 @@
     
-        $("#currentDay").text(moment().format("dddd, MMMM Do"))
+    
+       
+    $("#currentDay").text(moment().format("dddd, MMMM Do"))
+        
+    $("#currentTime").text(moment().format("ha"))
+        
+    $("#fw1").text(moment().add(1, 'hour').format("ha"))
+    $("#fw2").text(moment().add(2, 'hour').format("ha"))
+    $("#fw3").text(moment().add(3, 'hour').format("ha"))
+    $("#fw4").text(moment().add(4, 'hour').format("ha"))
+    $("#fw5").text(moment().add(5, 'hour').format("ha"))
+    $("#backOne").text(moment().subtract(1, 'hour').format("ha"))
+    $("#backTwo").text(moment().subtract(2, 'hour').format("ha"))
+    $("#backThree").text(moment().subtract(3, 'hour').format("ha"))
+
+        
+  
+
+    
+
     
 
