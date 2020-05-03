1e6135923960693758fc82bc9362d023839c4776
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-05-02 17:06:25 -0700

    added to javascript, storgeage code

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# 05-Third-Party-APIs-Scheduler
-var tenAm = document.querySelector("#10am");
-var elAm = document.querySelector("#11am");
-var twPm = document.querySelector("#12pm");
-var onePm = document.querySelector("#1pm");
-var twoPm = document.querySelector("#2pm");
-var threePM = document.querySelector("#3pm")
-var fourPM = document.querySelector("#4pm");
-var fivePM = document.querySelector("#5pm");
+
+  function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`.content`).text(value);
+    
+var saveBtn = $('.saveBtn');
+saveBtn.on('click', function(){
+    let eventId = $(this).attr('id');
+    let eventText = $(this).parent().siblings().children('.content').val();
+    localStorage.setItem(eventId, eventText);
+        })}}
--- java.js
+++ java.js
@@ -1,10 +1,8 @@
     
-    
+ 
        
-    $("#currentDay").text(moment().format("dddd, MMMM Do"))
-        
-    $("#currentTime").text(moment().format("ha"))
-        
+    $("#currentDay").text(moment().format("dddd, MMMM Do"))  
+    $("#currentTime").text(moment().format("ha")) 
     $("#fw1").text(moment().add(1, 'hour').format("ha"))
     $("#fw2").text(moment().add(2, 'hour').format("ha"))
     $("#fw3").text(moment().add(3, 'hour').format("ha"))
@@ -14,16 +12,23 @@
     $("#backTwo").text(moment().subtract(2, 'hour').format("ha"))
     $("#backThree").text(moment().subtract(3, 'hour').format("ha"))
 
-var currentHour = moment().format("h")
-    function colors(){ 
+    
+    var Info =[];
+
+    function addText(){
+        ev.preventDefault();  //to stop the form submitting
+        let newInfo = {
+            textarea: document.getElementById('textarea').value,
+            
+        }
+        Info.push(newInfo);
+        
+        //for display purposes only
         
 
-    };
-
-        
-  console.log(currentHour)
-
-    
-
-    
-
+        //saving to localStorage
+        localStorage.setItem('userInput', JSON.stringify(Info) );
+    }
+    document.addEventListener('.fas fa-save', ()=>{
+        document.getElementById('b1').addEventListener('click', '.saveBtn');
+    });
