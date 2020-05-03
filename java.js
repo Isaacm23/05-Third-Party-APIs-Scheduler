eb12408ad679c6f46ffe97e061e6f58f79934848
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-05-02 20:12:17 -0700

    added function to java

--- java.js
+++ java.js
@@ -1,34 +1,34 @@
-    
- 
-       
-    $("#currentDay").text(moment().format("dddd, MMMM Do"))  
-    $("#currentTime").text(moment().format("ha")) 
-    $("#fw1").text(moment().add(1, 'hour').format("ha"))
-    $("#fw2").text(moment().add(2, 'hour').format("ha"))
-    $("#fw3").text(moment().add(3, 'hour').format("ha"))
-    $("#fw4").text(moment().add(4, 'hour').format("ha"))
-    $("#fw5").text(moment().add(5, 'hour').format("ha"))
-    $("#backOne").text(moment().subtract(1, 'hour').format("ha"))
-    $("#backTwo").text(moment().subtract(2, 'hour').format("ha"))
-    $("#backThree").text(moment().subtract(3, 'hour').format("ha"))
 
-    
-    var Info =[];
+$(".saveBtn").on("click", function () {
+    console.log("button duh");
+    var click = $(this).attr("data-value");
+    var eventInput = $(click).val();
+    localStorage.setItem(click, eventInput);
+});
 
-    function addText(){
-        ev.preventDefault();  //to stop the form submitting
-        let newInfo = {
-            textarea: document.getElementById('textarea').value,
-            
-        }
-        Info.push(newInfo);
-        
-        //for display purposes only
-        
+// load from local storage on page load
 
-        //saving to localStorage
-        localStorage.setItem('userInput', JSON.stringify(Info) );
-    }
-    document.addEventListener('.fas fa-save', ()=>{
-        document.getElementById('b1').addEventListener('click', '.saveBtn');
-    });
+
+let timeBlock = ["#t1","#t2","#t3","#t4","#t5","#t6","#t7","#t8","#t9"]
+
+for (let i = 0; i < timeBlock.length; i++) {
+    let savedEvent = $('.saved-event')
+           
+    $(timeBlock[i]).val(localStorage.getItem(timeBlock[i]));
+};
+
+
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+$("#currentTime").text(moment().format("ha"));
+$("#fw1").text(moment().add(1, "hour").format("ha"));
+$("#fw2").text(moment().add(2, "hour").format("ha"));
+$("#fw3").text(moment().add(3, "hour").format("ha"));
+$("#fw4").text(moment().add(4, "hour").format("ha"));
+$("#fw5").text(moment().add(5, "hour").format("ha"));
+$("#backOne").text(moment().subtract(1, "hour").format("ha"));
+$("#backTwo").text(moment().subtract(2, "hour").format("ha"));
+$("#backThree").text(moment().subtract(3, "hour").format("ha"));
+
+
+
+
